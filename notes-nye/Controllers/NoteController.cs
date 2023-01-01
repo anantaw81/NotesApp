919ad7bd036e252f8737f0ff646c31c60a689dd6
anantaw81 <anantaw81@gmail.com>
2023-01-01 14:47:48 +0800

    Implemented fully CRUD on note model

--- notes-nye/Controllers/NoteController.cs
+++ notes-nye/Controllers/NoteController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using notes_nye.Data;
 using notes_nye.Models;
 
@@ -19,7 +20,7 @@ namespace notes_nye.Controllers
         {
             var notesList = dbContext.Notes.ToList();
             ViewData["notes"] = notesList;
-            return View();
+            return View(notesList);
         }
 
         // GET: NoteController/Details/5
@@ -51,20 +52,18 @@ namespace notes_nye.Controllers
 
             var notes = dbContext.Set<Note>();
             notes.Add(Tbl);
+
+            // Handle SavingData 
             int i = dbContext.SaveChanges();
             if (i > 0)
             {
                 ViewBag.Msg = "Data Saved Suuceessfully.";
             }
-            try
+            else
             {
-                return View();
-                return RedirectToAction(nameof(Index));
+                ViewBag.Msg = "Something Error at Saving Data.";
             }
-            catch
-            {
-                return View();
-            }
+            return View();
         }
 
         // GET: NoteController/Edit/5
@@ -78,6 +77,17 @@ namespace notes_nye.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, IFormCollection collection)
         {
+            var notesResult = dbContext.Notes.SingleOrDefault(x => x.Id == id);
+            if(notesResult != null)
+            {
+                notesResult.Id = id;
+                notesResult.Title = collection["noteTitle"];
+                notesResult.Content = collection["noteContent"];
+                notesResult.Writer = collection["noteWriter"];
+                notesResult.Tag = collection["noteTag"];
+                notesResult.Emotion = collection["noteEmotion"];
+                dbContext.SaveChanges();
+            }
             try
             {
                 return RedirectToAction(nameof(Index));
@@ -88,25 +98,72 @@ namespace notes_nye.Controllers
             }
         }
 
-        // GET: NoteController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
 
         // POST: NoteController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        [HttpGet]
+        /*[ValidateAntiForgeryToken]*/
+        public ActionResult Delete(int id)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            var notesResult = dbContext.Notes.Find(id);
+            dbContext.Notes.Remove(notesResult);
+            dbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
+
+        /*[HttpPost("{id}")]
+        public async Task<ActionResult<Note>> Delete(int id)
+        {
+            var notesResult = await dbContext.Notes.FindAsync(id);
+            if (notesResult == null)
+            {
+                return NotFound();
+            }
+            dbContext.Notes.Remove(notesResult);
+            await dbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }*/
+
+        [HttpGet]
+        public async Task<IActionResult> View(int id)
+        {
+            var notesResult = await dbContext.Notes.FirstOrDefaultAsync(x => x.Id == id);
+            if(notesResult != null)
+            {
+                return await Task.Run(() => View("View", notesResult));
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> View(Note model)
+        {
+            var notesResult = await dbContext.Notes.FindAsync(model.Id);
+            if(notesResult != null)
+            {
+                notesResult.Title = model.Title;
+                notesResult.Content = model.Content;
+                notesResult.Writer = model.Writer;
+                notesResult.Tag = model.Tag;
+                notesResult.Emotion = model.Emotion;
+                await dbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Note model)
+        {
+            var notesResult = await dbContext.Notes.FindAsync(model.Id);
+            if(notesResult != null)
+            {
+                dbContext.Notes.Remove(notesResult);
+                await dbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
